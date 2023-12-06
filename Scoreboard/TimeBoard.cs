2025364176bc6f8f8c9ec31cbdc81e1b4bdab76d
Mika1011 <137994556+Mika1011@users.noreply.github.com>
2023-12-06 21:43:25 +0100

    Update TimeBoard.cs

--- Scoreboard/TimeBoard.cs
+++ Scoreboard/TimeBoard.cs
@@ -22,7 +22,6 @@ namespace Game.ScoreBoards
             updateUI(savedTimes);
         }
 
-        [ContextMenu("Add Test Entry")]
         public void addEntryToBoard()
         {
             ScoreBoardEntryData entryData = new ScoreBoardEntryData
